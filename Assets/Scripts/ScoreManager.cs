af098c2bd27cf73d4209b289be8e8d920ffa2e69
zehrarahimian <zehrarahimian@gmail.com>
2023-01-13 16:54:15 +0330

    Finish score manager

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,25 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public int score =0;
+    public Text scoreText;
 
     void Start()
     {
-        
+        StartCoroutine(Score()); 
     }
 
 
     void Update()
     {
-        
+        scoreText.text = ("Score: " + score.ToString());
     }
 
-    IEnumerator score(){
-        yield return new WaitForSeconds(2);
-        score = score + 1;
-        Debug.Log("Score: " + score);
+    IEnumerator Score(){
+        while (true)
+        {
+          yield return new WaitForSeconds(2);
+          score = score + 1;  
+        }
     }
 }
